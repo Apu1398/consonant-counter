189cc7df3a5683dc7a59a7898f0f453259ea10b8
Apu1398 <jp.1398@hotmail.com>
2022-08-23 02:35:31 -0600

    The client send a file and receive how many consonants have

--- Client/client.c
+++ Client/client.c
@@ -1,16 +1,27 @@
+/*Pasos para compilar y ejecutar:
+1. "gcc -o client client.c"
+2. "./client ip puerto"*/
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <strings.h>
-#include <fcntl.h> // for open
+#include <fcntl.h>  // for open
 #include <unistd.h> // for close
-#include <netdb.h> //netbd.h es necesitada por la estructura hostent
+#include <netdb.h>  //netbd.h es necesitada por la estructura hostent
+#include<string.h>
+
+#define BUFFSIZE 1
+#define	ERROR	-1
+
+char buffer[BUFFSIZE];
+char *text;
 
 int main(int argc, char *argv[])
 {
-
+    //Lee argumento 2 (puerto) y empieza a setear el socket
     if (argc > 2)
     {
 
@@ -18,7 +29,7 @@ int main(int argc, char *argv[])
         int fd, numbytes, puerto;
         char buf[100];
         puerto = atoi(argv[2]);
-        ip = argv[1];
+        ip = argv[1];    //Aca lee el argumento 1 (ip)
 
         struct hostent *he;
         struct sockaddr_in server;
@@ -35,13 +46,12 @@ int main(int argc, char *argv[])
             exit(-1);
         }
 
-        //Datos del servidor
+        // Datos del servidor
         server.sin_family = AF_INET;
         server.sin_port = htons(puerto);
         server.sin_addr = *((struct in_addr *)he->h_addr);
         bzero(&(server.sin_zero), 8);
-
-        // Paso 3, conectarnos al servidor
+        
         if (connect(fd, (struct sockaddr *)&server,
                     sizeof(struct sockaddr)) == -1)
         {
@@ -49,17 +59,32 @@ int main(int argc, char *argv[])
             exit(-1);
         }
 
-        if ((numbytes = recv(fd, buf, 100, 0)) == -1)
+        //*****Abriendo el archivo y enviando****
+        //Aca habria que implementar logica para que "test.txt" no este quemado
+
+        FILE *archivo = fopen("test.txt", "rb");
+        if (!archivo)
         {
-            printf("Error en recv() \n");
-            exit(-1);
+            perror("Error al abrir el archivo:");
+            exit(EXIT_FAILURE);
         }
+        /*Se envia el archivo*/
+        fread(buffer, sizeof(char), BUFFSIZE, archivo);
+        while (!feof(archivo))
+        {
 
-        buf[numbytes] = '\0';
+            if (send(fd, buffer, BUFFSIZE, 0) == ERROR)
+                perror("Error al enviar el archivo:");
+            fread(buffer, sizeof(char), BUFFSIZE, archivo);
+        }
+        send(fd, "|", BUFFSIZE, 0); //Este caracter le indica al servidor que ya dejo de enviar caracteres. (Se puede cambiar por otro menos comun)
+        printf("Archivo enviado\n");
 
-        printf("Mensaje del Servidor: %s\n", buf);
+        recv(fd, buffer, 2, 0);  //Aca recibi la respuesta de cuantas consonantes hay. Se puede hacer lo mismo para cualquier otra info que se necesite
 
-        close(fd);
+        printf("Consonantes: %s\n", buffer);
+
+        close(fd);  //Cierra el socket
     }
     else
     {
